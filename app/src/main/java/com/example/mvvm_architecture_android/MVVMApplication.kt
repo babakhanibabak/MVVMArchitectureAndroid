b59f35bd38f417f72e0f680dbf49c1446bce6a1a
Babak <babakhanibabak@gmail.com>
2024-11-19 20:27:49 +0330

    Create MVVMApplication

---
+++ app/src/main/java/com/example/mvvm_architecture_android/MVVMApplication.kt
@@ -0,0 +1,9 @@
+package com.example.mvvm_architecture_android
+
+import android.app.Application
+
+class MVVMApplication:Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
