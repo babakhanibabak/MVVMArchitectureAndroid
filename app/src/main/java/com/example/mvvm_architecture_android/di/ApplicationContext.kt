d3e965795288dd9007b17403706afb9804a71a33
Babak <babakhanibabak@gmail.com>
2024-11-25 20:09:23 +0330

    Create ApplicationContext

---
+++ app/src/main/java/com/example/mvvm_architecture_android/di/ApplicationContext.kt
@@ -0,0 +1,16 @@
+package com.example.mvvm_architecture_android.di
+
+import javax.inject.Qualifier
+
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class ApplicationContext
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class ActivityContext
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class BaseUrl
